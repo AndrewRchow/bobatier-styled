8fa71cc428a7b567a0ee2985141e6415932d1de3
Andrew Chow <chowlick@gmail.com>
2019-07-01 10:15:15 -0700

    Updates

---
+++ precache-manifest.37edd861634c4afeaee87dac9419f555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d72a3181932b882cd5e3",
+    "url": "/bobatier-styled/static/css/main.a570b179.chunk.css"
+  },
+  {
+    "revision": "d72a3181932b882cd5e3",
+    "url": "/bobatier-styled/static/js/main.d72a3181.chunk.js"
+  },
+  {
+    "revision": "3d41cecb3b5cf7c9a707",
+    "url": "/bobatier-styled/static/css/1.0e3ba703.chunk.css"
+  },
+  {
+    "revision": "3d41cecb3b5cf7c9a707",
+    "url": "/bobatier-styled/static/js/1.3d41cecb.chunk.js"
+  },
+  {
+    "revision": "e9169ea91d2b0c6c4817",
+    "url": "/bobatier-styled/static/js/runtime~main.e9169ea9.js"
+  },
+  {
+    "revision": "70b82fb5eaf42967982532cd8710f15d",
+    "url": "/bobatier-styled/index.html"
+  }
+];
