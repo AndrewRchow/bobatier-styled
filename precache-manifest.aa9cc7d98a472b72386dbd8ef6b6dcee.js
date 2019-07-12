3b299f1bfc5e824cfe3159b704d9f736f2cc7665
Andrew <chowlick@gmail.com>
2019-07-12 16:32:27 -0700

    Updates

---
+++ precache-manifest.aa9cc7d98a472b72386dbd8ef6b6dcee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4707620fac276f3cc83",
+    "url": "/static/js/main.c4707620.chunk.js"
+  },
+  {
+    "revision": "af34b4799f47dc9e703c",
+    "url": "/static/js/1.af34b479.chunk.js"
+  },
+  {
+    "revision": "c4707620fac276f3cc83",
+    "url": "/static/css/main.b0b1f67c.chunk.css"
+  },
+  {
+    "revision": "af34b4799f47dc9e703c",
+    "url": "/static/css/1.96798fad.chunk.css"
+  },
+  {
+    "revision": "3775ed71c59f1515d458f443b9d704b5",
+    "url": "/index.html"
+  }
+];
