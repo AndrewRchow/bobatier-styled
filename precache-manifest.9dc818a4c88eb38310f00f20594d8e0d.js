ab3add9437194e383b5014754c9c6f77c9b28a6e
Andrew Chow <chowlick@gmail.com>
2019-08-26 17:50:09 -0700

    Updates

---
+++ precache-manifest.9dc818a4c88eb38310f00f20594d8e0d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "496932abe8443c1ccbe3",
+    "url": "/static/css/main.eaab93dc.chunk.css"
+  },
+  {
+    "revision": "496932abe8443c1ccbe3",
+    "url": "/static/js/main.496932ab.chunk.js"
+  },
+  {
+    "revision": "017b3f73cac2a0882a30",
+    "url": "/static/css/1.71cc83eb.chunk.css"
+  },
+  {
+    "revision": "017b3f73cac2a0882a30",
+    "url": "/static/js/1.017b3f73.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6225025d150431f2af60192fd668697d",
+    "url": "/static/media/shiba.6225025d.jpg"
+  },
+  {
+    "revision": "2999ca820f5b303df0797358dec35360",
+    "url": "/static/media/coffee.2999ca82.svg"
+  },
+  {
+    "revision": "387b52e5e63de3e8a6d7b60aa6d0d976",
+    "url": "/static/media/people.387b52e5.svg"
+  },
+  {
+    "revision": "a3a8fc71dcb0ee9f2fb2cf96a4651f78",
+    "url": "/static/media/photo.a3a8fc71.svg"
+  },
+  {
+    "revision": "17f38dc420c62d7b94327c12b2d45902",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.781d5971c64e01463fffbede831e9993.js"
+  "/precache-manifest.9dc818a4c88eb38310f00f20594d8e0d.js"
 );
 
 workbox.clientsClaim();
